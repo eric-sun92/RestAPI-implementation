16b0739212306f3adb224ca9e4a82e83f9160cb6
Eric <eric.sun.ecs92@yale.edu>
2022-07-23 16:09:30 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('connected to database'))
+
+// app.get('/', asnyc (req, res) => {
+//     res.render('index.js')
+
+// })
+
+app.use(express.json())
+
+const subscribersRouter = require('./routes/subscribers')
+
+app.use('/subscribers', subscribersRouter)
+
+app.listen(3000, () => {
+    console.log('server started')
+})
+
+//render the register page
+//get the post req and its contents
+//u need to store hashsed passwords into the db 
+
+//render the login page
+//get the post req and find the matching one in database
+//pass in req.body.password and hashedPassword and compare them
+
+
